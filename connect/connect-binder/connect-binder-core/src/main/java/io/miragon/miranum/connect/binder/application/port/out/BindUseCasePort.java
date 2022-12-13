676c059cde0f6b300e7a98b9f45714a4551ef725
Dominik Horn <dominik.horn93@gmail.com>
2022-12-13 13:52:07 +0100

    fix: .gitignore

---
+++ connect/connect-binder/connect-binder-core/src/main/java/io/miragon/miranum/connect/binder/application/port/out/BindUseCasePort.java
@@ -0,0 +1,9 @@
+package io.miragon.miranum.connect.binder.application.port.out;
+
+import io.miragon.miranum.connect.binder.domain.UseCaseInfo;
+
+public interface BindUseCasePort {
+
+    void bind(UseCaseInfo useCaseInfo);
+
+}
