f666ea158d31d366925af45cc08bbd0ebd39261e
Dominik Horn <dominik.horn93@gmail.com>
2022-12-14 09:17:42 +0100

    feat: add mail example

---
+++ integrations/mail/mail-core/src/main/java/io/miragon/miranum/integrations/mail/MailProperties.java
@@ -0,0 +1,16 @@
+package io.miragon.miranum.integrations.mail;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "miranum.mail")
+public class MailProperties {
+
+
+    /**
+     * Type of output.
+     * Currently mail or sendgrid
+     */
+    public String output;
+
+}
+
---
+++ integrations/mail/mail-core/src/main/java/io/miragon/miranum/integrations/mail/application/port/in/SendMailCommand.java
@@ -0,0 +1,11 @@
+package io.miragon.miranum.integrations.mail.application.port.in;
+
+public class SendMailCommand {
+
+    private String text;
+
+    private String subject;
+
+    private String recipient;
+
+}
