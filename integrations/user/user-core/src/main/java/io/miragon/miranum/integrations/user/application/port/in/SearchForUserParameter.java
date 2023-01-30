248f83730b00d260fb214bf89372a25295bba979
Rami Nakkar <rami.nakkar@miragon.io>
2023-01-30 14:40:19 +0100

    Added no args constructor

--- integrations/user/user-core/src/main/java/io/miragon/miranum/integrations/user/application/port/in/SearchForUserParameter.java
+++ integrations/user/user-core/src/main/java/io/miragon/miranum/integrations/user/application/port/in/SearchForUserParameter.java
@@ -2,9 +2,11 @@ package io.miragon.miranum.integrations.user.application.port.in;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SearchForUserParameter {
     private String userId;
 }
