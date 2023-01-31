fdca3c3d6c34c5dbc3d006c5ba7f5e179311e5b3
Rami Nakkar <rami.nakkar@miragon.io>
2023-01-31 19:49:48 +0100

    replaced imported config by autoconfig

--- integrations/user/user-c7/src/main/java/io/miragon/miranum/integrations/example/c7/UserC7Application.java
+++ integrations/user/user-c7/src/main/java/io/miragon/miranum/integrations/example/c7/UserC7Application.java
@@ -1,12 +1,9 @@
 package io.miragon.miranum.integrations.example.c7;
 
-import io.miragon.miranum.integrations.user.UserConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(UserConfiguration.class)
 public class UserC7Application {
 
     public static void main(final String[] args) {
