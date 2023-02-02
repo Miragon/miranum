0aa18f3aef1666ffa537962895336b8b79e25cef
Thomas Heinrichs <52525.thomas@gmail.com>
2023-02-02 16:11:38 +0100

    Added structure to the public plan demo example module

---
+++ examples/public-plan-demo/public-plan-demo-camunda-7/src/main/java/io/miragon/publicplandemocamunda8/PublicPlanDemoCamunda7Application.java
@@ -0,0 +1,16 @@
+package io.miragon.publicplandemocamunda8;
+
+import io.miragon.publicplandemocore.PublicPlanDemoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(PublicPlanDemoConfiguration.class)
+public class PublicPlanDemoCamunda7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicPlanDemoCamunda7Application.class, args);
+    }
+
+}
---
+++ examples/public-plan-demo/public-plan-demo-core/src/main/java/io/miragon/publicplandemocore/PublicPlanDemoConfiguration.java
@@ -0,0 +1,25 @@
+package io.miragon.publicplandemocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PublicPlanDemoConfiguration {
+
+    @Bean
+    public void generatePDFUseCase(){
+
+    }
+
+    @Bean
+    public void storePDFUseCase(){
+
+    }
+
+    @Bean
+    public void tenantInterceptor(){
+
+    }
+
+}
