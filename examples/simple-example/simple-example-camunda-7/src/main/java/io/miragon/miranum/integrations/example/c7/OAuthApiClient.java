ffb885308e4c4471f50f07531c9ac58188ce3f83
Dominik Horn <dominik.horn93@gmail.com>
2023-02-06 17:54:16 +0100

    feat: auth interceptor

---
+++ examples/simple-example/simple-example-camunda-7/src/main/java/io/miragon/miranum/integrations/example/c7/OAuthApiClient.java
@@ -0,0 +1,11 @@
+package io.miragon.miranum.integrations.example.c7;
+
+import org.camunda.community.rest.client.invoker.ApiClient;
+
+public class OAuthApiClient extends ApiClient {
+
+    @Override
+    public void setAccessToken(final String accessToken) {
+        //
+    }
+}
