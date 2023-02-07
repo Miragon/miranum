f039cc1e1929aaaacc9081f4f5a74359b2c83366
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-07 10:17:21 +0100

    fix: removed @author tags

--- integrations/user/user-adapter/rest-adapter/src/main/java/io/miragon/miranum/integrations/example/api/transport/UserTO.java
+++ integrations/user/user-adapter/rest-adapter/src/main/java/io/miragon/miranum/integrations/example/api/transport/UserTO.java
@@ -5,50 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * User transport object.
- *
- * @author externer.dl.horn
- */
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserTO {
 
-    /**
-     * unique id of the user
-     */
-    private String lhmObjectId;
-
-    /**
-     * Username
-     */
     private String username;
 
-    /**
-     * Forname
-     */
-    private String forename;
+    private String id;
 
-    /**
-     * Surname
-     */
+    private String firstname;
+
     private String surname;
 
-    /**
-     * Group of the user
-     */
-    private String ou;
-
-    /**
-     * Department of the user
-     */
-    private String department;
-
-    /**
-     * Email address
-     */
     private String email;
 
+    private String title;
+
+    private String department;
+
+    private List<String> groups;
 }
--- integrations/user/user-core/src/main/java/io/miragon/miranum/integrations/user/common/AppAuthenticationProvider.java
+++ integrations/user/user-core/src/main/java/io/miragon/miranum/integrations/user/common/AppAuthenticationProvider.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 /**
  * Get current camunda authentication information.
- *
- * @author externer.dl.horn
  */
 @Component
 @RequiredArgsConstructor
