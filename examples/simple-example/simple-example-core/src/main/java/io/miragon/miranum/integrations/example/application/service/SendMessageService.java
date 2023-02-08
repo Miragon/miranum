85a7f4d9c2da4cd567430d2ca71b40839d30cb07
Dominik Horn <dominik.horn93@gmail.com>
2023-02-08 14:27:07 +0100

    fix: remove message endpoints

--- examples/simple-example/simple-example-core/src/main/java/io/miragon/miranum/integrations/example/application/service/SendMessageService.java
+++ examples/simple-example/simple-example-core/src/main/java/io/miragon/miranum/integrations/example/application/service/SendMessageService.java
@@ -1,6 +1,5 @@
 package io.miragon.miranum.integrations.example.application.service;
 
-import io.miragon.miranum.connect.message.application.port.in.CorrelateMessageUseCase;
 import io.miragon.miranum.connect.worker.domain.Worker;
 import io.miragon.miranum.integrations.example.application.port.in.SendMessageCommand;
 import io.miragon.miranum.integrations.example.application.port.in.SendMessageUseCase;
@@ -12,13 +11,10 @@ import lombok.extern.java.Log;
 @RequiredArgsConstructor
 public class SendMessageService implements SendMessageUseCase {
 
-    private final CorrelateMessageUseCase correlateMessageUseCase;
-
     @Override
     @Worker(type = "sendMessage")
     public Answer sendMessage(final SendMessageCommand message) {
         log.info("Received message: " + message);
-        //   correlateMessageUseCase.correlateMessage(new CorrelateMessageCommand(message.getName(), message.getKey(), Map.of()));
         return new Answer("answer to: " + message.getName());
     }
 }
