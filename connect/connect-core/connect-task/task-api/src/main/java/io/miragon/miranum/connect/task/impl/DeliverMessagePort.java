3aad40d99d533a38fd071687b3ae1f080dd16c9a
Dominik Horn <dominik.horn93@gmail.com>
2023-02-13 17:09:07 +0100

    feat: add task api

--- connect/connect-core/connect-task/task-api/src/main/java/io/miragon/miranum/connect/task/impl/DeliverMessagePort.java
+++
@@ -1,9 +0,0 @@
-package io.miragon.miranum.connect.message.impl;
-
-import io.miragon.miranum.connect.message.api.CorrelateMessageCommand;
-
-public interface DeliverMessagePort {
-
-    void deliverMessage(CorrelateMessageCommand command) throws MessageCorrelationException;
-
-}
