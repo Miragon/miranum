74490d48a5ec5f0ce68ff7c89d7196bee29a5173
Dominik Horn <dominik.horn93@gmail.com>
2023-02-13 17:45:03 +0100

    feat: init cloud event gateway

---
+++ connect/connect-c7/c7-cloudevents-gateway/src/main/java/io/miragon/miranum/integrations/example/c7/ExampleC7Application.java
@@ -0,0 +1,13 @@
+package io.miragon.miranum.integrations.example.c7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleC7Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ExampleC7Application.class, args);
+    }
+
+}
