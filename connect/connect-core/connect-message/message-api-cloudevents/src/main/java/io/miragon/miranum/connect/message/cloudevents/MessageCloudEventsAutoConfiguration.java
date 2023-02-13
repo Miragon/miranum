b81f2f4feec5f7d2c49ef6d1968b8390545cf68e
Dominik Horn <dominik.horn93@gmail.com>
2023-02-13 17:38:26 +0100

    feat: add message cloud events module

---
+++ connect/connect-core/connect-message/message-api-cloudevents/src/main/java/io/miragon/miranum/connect/message/cloudevents/MessageCloudEventsAutoConfiguration.java
@@ -0,0 +1,11 @@
+package io.miragon.miranum.connect.message.cloudevents;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageCloudEventsAutoConfiguration {
+
+
+}
+
