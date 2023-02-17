9db226e14f355cb58ac7d5194df8fc6088a22ab4
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-17 15:26:18 +0100

    feat: REST-Adapter to place order

---
+++ examples/pizza-order/waiter/src/main/java/io/miragon/miranum/examples/pizzaorder/waiter/adapter/in/rest/PlaceOrderController.java
@@ -0,0 +1,27 @@
+package io.miragon.miranum.examples.pizzaorder.waiter.adapter.in.rest;
+
+import io.miragon.miranum.examples.pizzaorder.waiter.application.port.in.PlaceOrderInCommand;
+import io.miragon.miranum.examples.pizzaorder.waiter.application.port.in.PlaceOrderUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/place-order")
+@AllArgsConstructor
+public class PlaceOrderController {
+
+    private final PlaceOrderUseCase placeOrderUseCase;
+
+    @PostMapping
+    public ResponseEntity<String> placeOrder(@RequestBody PlaceOrderRequestDto placeOrderRequestDto) {
+        var placeOrderInCommand = new PlaceOrderInCommand(placeOrderRequestDto.getEmail(),
+                placeOrderRequestDto.getFood(), placeOrderRequestDto.getDrinks());
+        placeOrderUseCase.placeOrder(placeOrderInCommand);
+        // TODO: Should return OrderObject
+        return ResponseEntity.ok("successful");
+    }
+}
