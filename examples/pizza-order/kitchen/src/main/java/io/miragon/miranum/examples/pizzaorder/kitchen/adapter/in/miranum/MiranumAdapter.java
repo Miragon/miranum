d86da55c5df5af85b1bb0f9069a94cd057265729
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-17 14:13:38 +0100

    feat: add miranum-kitchen-in-adapter

---
+++ examples/pizza-order/kitchen/src/main/java/io/miragon/miranum/examples/pizzaorder/kitchen/adapter/in/miranum/MiranumAdapter.java
@@ -0,0 +1,17 @@
+package io.miragon.miranum.examples.pizzaorder.kitchen.adapter.in.miranum;
+
+import io.miragon.miranum.connect.worker.api.Worker;
+import io.miragon.miranum.examples.pizzaorder.kitchen.application.port.in.PreparePizzaCommand;
+import io.miragon.miranum.examples.pizzaorder.kitchen.application.port.in.PreparePizzaUseCase;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class MiranumAdapter {
+
+    private final PreparePizzaUseCase preparePizzaUseCase;
+
+    @Worker(type = "preparePizza")
+    public void preparePizza(PreparePizzaCommand preparePizzaCommand) {
+        preparePizzaUseCase.preparePizza(preparePizzaCommand);
+    }
+}
