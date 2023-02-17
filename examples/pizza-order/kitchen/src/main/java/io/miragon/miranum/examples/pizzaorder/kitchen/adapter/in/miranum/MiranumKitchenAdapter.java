8f0718eb422b80afc4317da1a3d29ba24495b8d0
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-17 15:48:43 +0100

    feat: add autoconfigs for kitchen service

--- examples/pizza-order/kitchen/src/main/java/io/miragon/miranum/examples/pizzaorder/kitchen/adapter/in/miranum/MiranumKitchenAdapter.java
+++ examples/pizza-order/kitchen/src/main/java/io/miragon/miranum/examples/pizzaorder/kitchen/adapter/in/miranum/MiranumKitchenAdapter.java
@@ -6,7 +6,7 @@ import io.miragon.miranum.examples.pizzaorder.kitchen.application.port.in.Prepar
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class MiranumAdapter {
+public class MiranumKitchenAdapter {
 
     private final PreparePizzaUseCase preparePizzaUseCase;
 
