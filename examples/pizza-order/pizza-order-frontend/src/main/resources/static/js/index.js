8bdf3c69853957a732065173af7db287cbfd2ac0
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-22 15:46:06 +0100

    feat: clear inputs after submit

--- examples/pizza-order/pizza-order-frontend/src/main/resources/static/js/index.js
+++ examples/pizza-order/pizza-order-frontend/src/main/resources/static/js/index.js
@@ -21,8 +21,13 @@ $("#form").submit(async function (event) {
     response.text().then(response => {
         const text = `Your order was ${response}, you will be notified soon!`;
         $('.response').append('<br><p style="border:2px solid green; text-align: center">' + text + '</p>');
+        cleanInputs();
     }).catch(err => {
         console.error(err);
         $('.response').append('<br><p style="border:2px solid red; text-align: center">' + err + '</p>');
     })
 });
+
+const cleanInputs = () => {
+    $('#form')[0].reset();
+}
