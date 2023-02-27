7d38f9b4d2f786942c75fcd1f9cad483e6b95bd4
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-27 13:08:51 +0100

    feat: add openapi generated fit connect submission api client

---
+++ integrations/fit-connect-client/src/main/java/Test.java
@@ -0,0 +1,7 @@
+import io.miragon.miranum.fitconnect.client.api.EinreichungsbermittlungApi;
+
+public class Test {
+    public static void main(String[] args) {
+        EinreichungsbermittlungApi api = new EinreichungsbermittlungApi();
+    }
+}
