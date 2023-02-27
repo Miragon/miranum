67bba50198f0849a0ac9a9a70a07874db14281a6
Julian Haupt <julian@haupt.media>
2023-02-27 23:40:49 +0100

    allow multiple CloudEventHandlers to be registered

--- connect/connect-cloudevents/cloudevents-spring-webmvc/src/main/java/io/miragon/miranum/connect/cloudevents/CloudEventsAutoConfiguration.java
+++ connect/connect-cloudevents/cloudevents-spring-webmvc/src/main/java/io/miragon/miranum/connect/cloudevents/CloudEventsAutoConfiguration.java
@@ -1,15 +1,21 @@
 package io.miragon.miranum.connect.cloudevents;
 
 import io.cloudevents.spring.mvc.CloudEventHttpMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class CloudEventsAutoConfiguration implements WebMvcConfigurer {
+    @Autowired
+    private List<CloudEventHandler> ceHandlers;
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(0, new CloudEventHttpMessageConverter());
@@ -17,7 +23,10 @@ public class CloudEventsAutoConfiguration implements WebMvcConfigurer {
 
     @Bean
     public CloudEventsController cloudEventsController() {
-        return new CloudEventsController();
+        // build up a lookup hash map for all registered CloudEventHandler Beans
+        Map<String, CloudEventHandler> ceHandlerMap = new HashMap<>();
+        ceHandlers.forEach(ceHandler -> ceHandlerMap.put(ceHandler.getType(), ceHandler));
+        return new CloudEventsController(ceHandlerMap);
     }
 
 }
--- connect/connect-cloudevents/cloudevents-spring-webmvc/src/main/java/io/miragon/miranum/connect/cloudevents/CloudEventsController.java
+++ connect/connect-cloudevents/cloudevents-spring-webmvc/src/main/java/io/miragon/miranum/connect/cloudevents/CloudEventsController.java
@@ -5,20 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("cloudevents")
 public class CloudEventsController {
-    @Autowired
-    CloudEventHandler cloudEventHandler;
+    Map<String, CloudEventHandler> handlerMap;
+
+    public CloudEventsController(Map<String, CloudEventHandler> handlerMap) {
+        this.handlerMap = handlerMap;
+    }
 
     @PostMapping(value = "/")
     public CloudEvent consumeCloudEvent(@RequestBody CloudEvent cloudEvent) throws IOException {
-        if (!cloudEvent.getType().equals(cloudEventHandler.getType()))
+        if (!handlerMap.containsKey(cloudEvent.getType()))
             throw new IllegalStateException("Unsupported CloudEvent Type: " + cloudEvent.getType());
 
-         return cloudEventHandler.handleCloudEvent(cloudEvent)
+         return handlerMap.get(cloudEvent.getType())
+                 .handleCloudEvent(cloudEvent)
                  .orElse(null);
     }
 }
