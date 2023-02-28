d07e09c79ab2244f9cd555cf42fc02d4fe0e1131
Rami Nakkar <rami.nakkar@miragon.io>
2023-02-28 14:04:43 +0100

    fix: invalidate and remove destinationid

--- integrations/fit-connect-online-service-client/src/main/java/io/miragon/miraum/fitconnect/integration/onlineservice/OnlineServiceApplication.java
+++ integrations/fit-connect-online-service-client/src/main/java/io/miragon/miraum/fitconnect/integration/onlineservice/OnlineServiceApplication.java
@@ -16,11 +16,7 @@ public class OnlineServiceApplication {
         ConfigurableApplicationContext context = SpringApplication.run(OnlineServiceApplication.class, args);
 
         var api = context.getBean(EinreichungsbermittlungApi.class);
-        var info = api.getDestinationInfo(UUID.fromString("e61f0d18-5bbd-46da-9dba-6abfe97604de")).block();
+        var info = api.getDestinationInfo(UUID.fromString("<destination-id>")).block();
         log.info(info.toString());
-//        var submission = new CreateSubmission();
-//        submission.destinationId(UUID.fromString("e61f0d18-5bbd-46da-9dba-6abfe97604de"));
-//        var response = api.createSubmissionWithResponseSpec(submission);
-//        System.out.println(response);
     }
 }
