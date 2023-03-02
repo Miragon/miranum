947a3242cbaf010e67aeff08762e8868049a2055
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-02 10:25:59 +0100

    refactor: extract RSAService and ProcessCommandFactory from AuthorityApiImpl

--- integrations/fit-connect/fit-connect-authority-client/src/main/java/io/miragon/miraum/fitconnect/integration/authority/api/AuthorityApi.java
+++ integrations/fit-connect/fit-connect-authority-client/src/main/java/io/miragon/miraum/fitconnect/integration/authority/api/AuthorityApi.java
@@ -1,11 +1,9 @@
 package io.miragon.miraum.fitconnect.integration.authority.api;
 
-import com.nimbusds.jose.JOSEException;
-
 import java.io.IOException;
 import java.text.ParseException;
 
 public interface AuthorityApi {
 
-    void pollAndAcceptPickupReadySubmissions() throws JOSEException, ParseException, IOException;
+    void pollAndAcceptPickupReadySubmissions() throws ParseException, IOException;
 }
--- integrations/fit-connect/fit-connect-authority-client/src/main/java/io/miragon/miraum/fitconnect/integration/authority/impl/AuthorityApiImpl.java
+++ integrations/fit-connect/fit-connect-authority-client/src/main/java/io/miragon/miraum/fitconnect/integration/authority/impl/AuthorityApiImpl.java
@@ -1,16 +1,14 @@
 package io.miragon.miraum.fitconnect.integration.authority.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.RSADecrypter;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.KeyOperation;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import io.miragon.miranum.connect.process.api.ProcessApi;
-import io.miragon.miranum.connect.process.api.StartProcessCommand;
 import io.miragon.miraum.fitconnect.integration.authority.AuthorityProperties;
 import io.miragon.miraum.fitconnect.integration.authority.api.AuthorityApi;
 import io.miragon.miraum.fitconnect.integration.gen.api.EinreichungsempfangApi;
@@ -39,7 +37,7 @@ public class AuthorityApiImpl implements AuthorityApi {
     private final ProcessApi processApi;
 
     @Override
-    public void pollAndAcceptPickupReadySubmissions() throws JOSEException, ParseException, IOException {
+    public void pollAndAcceptPickupReadySubmissions() throws ParseException, IOException {
         log.info("Fetch submissions...");
         var submissionsForPickupResponse = apiClient.getSubmissionsForPickup(UUID.fromString(authorityProperties.getDestinationId()), 100, 0).block();
 
@@ -49,25 +47,16 @@ public class AuthorityApiImpl implements AuthorityApi {
 
             for (var attachmentId : submissionData.getAttachments()) {
                 var attachment = apiClient.getSubmissionAttachment(submission.getSubmissionId(), attachmentId).block();
+
                 log.info("Encrypted attachment payload: " + attachment);
 
-                var path = Paths.get(authorityProperties.getPrivateKeyDecryptionPath());
+                var rsaService = new RSAService(Paths.get(authorityProperties.getPrivateKeyDecryptionPath()));
+                rsaService.validateRsaKey();
+                var decryptedPayload = rsaService.decrypt(attachment);
 
-                String keyStr = new String(Files.readAllBytes(path));
-                RSAKey jwk = RSAKey.parse(keyStr);
+                log.info("Decrypted attachment payload: " + decryptedPayload);
 
-                validateRSAKey(jwk, true);
-
-                JWEObject jweObject = JWEObject.parse(attachment);
-
-                jweObject.decrypt(new RSADecrypter(jwk));
-
-                log.info("Decrypted attachment payload: " + jweObject.getPayload().toString());
-
-                var objectMapper = new ObjectMapper();
-                Map<String, Object> payloadAsMap = objectMapper.readValue(jweObject.getPayload().toString(), Map.class);
-
-                var startProcessCommand = new StartProcessCommand(authorityProperties.getProcessKey(), payloadAsMap);
+                var startProcessCommand = ProcessCommandFactory.create(decryptedPayload, authorityProperties.getProcessKey());
                 processApi.startProcess(startProcessCommand);
             }
 
@@ -77,17 +66,16 @@ public class AuthorityApiImpl implements AuthorityApi {
         }
     }
 
-    private String createSignedAndSerializedAcceptSet(String destinationIdStr, SubmissionForPickup submission) throws ParseException, IOException {
+    private String createSignedAndSerializedAcceptSet(String destinationIdStr, SubmissionForPickup submission) throws IOException, ParseException {
         JWK signaturePrivateKey = RSAKey.parse(Files.readString(Paths.get(authorityProperties.getPrivateKeySigningPath())));
-        String issuer = destinationIdStr; // our own destinationId (MUST match destinationId of this submission)
         String subject = "submission:" + submission.getSubmissionId();
-        String event ="https://schema.fitko.de/fit-connect/events/accept-submission";
+        String event = "https://schema.fitko.de/fit-connect/events/accept-submission";
         String transactionId = "case:" + submission.getCaseId();
 
         try {
             JWSSigner signer = new RSASSASigner(signaturePrivateKey.toRSAKey());
             JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                    .issuer(issuer)
+                    .issuer(destinationIdStr)
                     .issueTime(new Date())
                     .jwtID(UUID.randomUUID().toString())
                     .subject(subject)
@@ -110,27 +98,6 @@ public class AuthorityApiImpl implements AuthorityApi {
             return signedJWT.serialize();
         } catch (JOSEException e) {
             throw new RuntimeException("Could not generate SET");
-        }
-    }
-
-    public static void validateRSAKey(RSAKey RSAKey, boolean isPrivate) {
-        validateTrueOrElseThrow((RSAKey.getModulus().decodeToBigInteger().bitLength() >= 4096), "JWK has wrong key length.");
-        if (isPrivate) {
-            validateTrueOrElseThrow(RSAKey.getKeyOperations().size() == 1 &&
-                            RSAKey.getKeyOperations().contains(KeyOperation.UNWRAP_KEY),
-                    "The specified private key is not intended for 'unwrapKey' as specified through key operation.");
-        } else {
-            validateTrueOrElseThrow(RSAKey.getKeyOperations().size() == 1 &&
-                            RSAKey.getKeyOperations().contains(KeyOperation.WRAP_KEY),
-                    "The specified public key is not intended for 'wrapKey' as specified through key operation.");
-        }
-        validateTrueOrElseThrow(RSAKey.getAlgorithm().equals(JWEAlgorithm.RSA_OAEP_256), "Key algorithm must be RSA-OAEP-256!");
-        validateTrueOrElseThrow(RSAKey.getPublicExponent().toString().equals("AQAB"), "Key must have e: \"AQAB\"");
-    }
-
-    private static void validateTrueOrElseThrow(boolean expression, String msg) {
-        if (!expression) {
-            throw new RuntimeException(msg);
         }
     }
 }
