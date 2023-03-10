8a14876ccbbacda25ad06c1d0f6bd72661f40068
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-10 09:51:19 +0100

    refactor: use constructor injection instead of property

--- examples/simple-example/simple-example-camunda-7/src/main/java/io/miragon/miranum/integrations/example/c7/ExampleC7Application.java
+++ examples/simple-example/simple-example-camunda-7/src/main/java/io/miragon/miranum/integrations/example/c7/ExampleC7Application.java
@@ -2,18 +2,18 @@ package io.miragon.miranum.integrations.example.c7;
 
 import io.miragon.miranum.integrations.example.ExampleConfiguration;
 import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
 import org.camunda.bpm.client.ExternalTaskClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(ExampleConfiguration.class)
+@AllArgsConstructor
 public class ExampleC7Application {
 
-    @Autowired
-    private ExternalTaskClient client;
+    private final ExternalTaskClient client;
 
     public static void main(final String[] args) {
         SpringApplication.run(ExampleC7Application.class, args);
@@ -23,5 +23,4 @@ public class ExampleC7Application {
     public void init() {
         this.client.isActive();
     }
-
 }
