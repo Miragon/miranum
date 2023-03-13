0043f29fed97d6cff7d4d7a58a8fcfd6df4a693b
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-13 20:20:07 +0100

    docu: add javadoc to element template generation anno

--- connect/connect-core/connect-element-templates/element-template-api/src/main/java/io/miragon/miranum/connect/elementtemplate/api/GenerateElementTemplate.java
+++ connect/connect-core/connect-element-templates/element-template-api/src/main/java/io/miragon/miranum/connect/elementtemplate/api/GenerateElementTemplate.java
@@ -5,12 +5,44 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to generate an element template from a worker definition.
+ * The fields of the input and output parameter of the method can be annotated
+ * with {@link ElementTemplateProperty} to gain additional control over the element
+ * template generation.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface GenerateElementTemplate {
+
+    /**
+     * The name of the element template.
+     * Corresponds to the name property of the element template.
+     * @return the name of the element template
+     */
     String name();
+    /**
+     * The id of the element template.
+     * Corresponds to the id property of the element template.
+     * @return the id of the element template
+     */
     String id();
+    /**
+     * The type of the element template.
+     * Corresponds to the topic of the external task.
+     * @return the description of the element template
+     */
     String type();
+    /**
+     * The BPMN element types to which the element template applies.
+     * Corresponds to the appliesTo property of the element template.
+     * @return the BPMN element types to which the element template applies
+     */
     BPMNElementType[] appliesTo() default {BPMNElementType.BPMN_SERVICE_TASK};
+    /**
+     * The version of the element template.
+     * Corresponds to the version property of the element template.
+     * @return the version of the element template
+     */
     double version();
 }
