c2aeb53155aa74f8c0695f6805b6426e3185002a
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-13 19:07:44 +0100

    refactor: remove old element teplate and replace with gen

--- connect/connect-c7/element-templates-c7/src/main/java/io/miragon/miranum/connect/adapter/in/c7/elementtemplates/Camunda7ElementTemplateGenerator.java
+++ connect/connect-c7/element-templates-c7/src/main/java/io/miragon/miranum/connect/adapter/in/c7/elementtemplates/Camunda7ElementTemplateGenerator.java
@@ -2,7 +2,10 @@ package io.miragon.miranum.connect.adapter.in.c7.elementtemplates;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.miragon.miranum.connect.adapter.in.c7.elementtemplates.model.*;
+import io.miragon.miranum.connect.c7.elementtemplates.gen.Constraints;
+import io.miragon.miranum.connect.c7.elementtemplates.gen.ElementTemplatesJsonSchema010;
+import io.miragon.miranum.connect.c7.elementtemplates.gen.Property;
+import io.miragon.miranum.connect.c7.elementtemplates.gen.Binding;
 import io.miragon.miranum.connect.elementtemplate.api.BPMNElementType;
 import io.miragon.miranum.connect.elementtemplate.api.ElementTemplateProperty;
 import io.miragon.miranum.connect.elementtemplate.api.PropertyType;
@@ -17,26 +20,36 @@ import java.util.Objects;
 @Log
 public class Camunda7ElementTemplateGenerator implements GenerateElementTemplatePort {
 
-    private static final String SCHEMA = "https://unpkg.com/@camunda/element-templates-json-schema@0.10.0/resources/schema.json";
 
     @Override
     public ElementTemplateGenerationResult generate(ElementTemplateInfo elementTemplateInfo) {
-        var elementTemplate = new Camunda7ElementTemplate();
+        var elementTemplate = new ElementTemplatesJsonSchema010();
         elementTemplate.setName(elementTemplateInfo.getName());
         elementTemplate.setId(elementTemplateInfo.getId());
         elementTemplate.setAppliesTo(Arrays.stream(elementTemplateInfo.getAppliesTo()).map(BPMNElementType::getValue).toList());
-        elementTemplate.setSchema(SCHEMA);
 
         // Add external task property
-        var implementationProperty = new Property("Implementation Type", PropertyType.STRING, "external");
+        var implementationProperty = new Property();
+        implementationProperty.setLabel("Implementation Type");
+        implementationProperty.setType(PropertyType.STRING.getType());
+        implementationProperty.setValue("external");
         implementationProperty.setEditable(false);
-        var implementationBinding = new Binding(BindingType.PROPERTY, "camunda:type");
+
+        var implementationBinding = new Binding();
+        implementationBinding.setType(Binding.Type.PROPERTY);
+        implementationBinding.setName("camunda:type");
         implementationProperty.setBinding(implementationBinding);
         elementTemplate.getProperties().add(implementationProperty);
 
         // Add property for the topic of the external task
-        var implementationTopicProperty = new Property("Topic", PropertyType.STRING, elementTemplateInfo.getType());
-        var implementationTopicBinding = new Binding(BindingType.PROPERTY, "camunda:topic");
+        var implementationTopicProperty = new Property();
+        implementationTopicProperty.setLabel("Topic");
+        implementationTopicProperty.setType(PropertyType.STRING.getType());
+        implementationTopicProperty.setValue(elementTemplateInfo.getType());
+
+        var implementationTopicBinding = new Binding();
+        implementationTopicBinding.setType(Binding.Type.PROPERTY);
+        implementationTopicBinding.setName("camunda:topic");
         implementationTopicProperty.setBinding(implementationTopicBinding);
         elementTemplate.getProperties().add(implementationTopicProperty);
 
@@ -50,7 +63,9 @@ public class Camunda7ElementTemplateGenerator implements GenerateElementTemplate
                         "",
                         field.getAnnotation(ElementTemplateProperty.class));
 
-                var binding = new Binding(BindingType.INPUT_PARAMETER, field.getName());
+                var binding = new Binding();
+                binding.setType(Binding.Type.CAMUNDA_INPUT_PARAMETER);
+                binding.setName(field.getName());
 
                 property.setBinding(binding);
                 elementTemplate.getProperties().add(property);
@@ -67,7 +82,9 @@ public class Camunda7ElementTemplateGenerator implements GenerateElementTemplate
                         field.getName() + "Result",
                         field.getAnnotation(ElementTemplateProperty.class));
 
-                var binding = new Binding(BindingType.OUTPUT_PARAMETER, field.getName(), "");
+                var binding = new Binding();
+                binding.setType(Binding.Type.CAMUNDA_OUTPUT_PARAMETER);
+                binding.setSource(field.getName());
 
                 property.setBinding(binding);
                 elementTemplate.getProperties().add(property);
@@ -77,7 +94,7 @@ public class Camunda7ElementTemplateGenerator implements GenerateElementTemplate
         return new ElementTemplateGenerationResult(convertToJsonString(elementTemplate));
     }
 
-    private String convertToJsonString(Camunda7ElementTemplate elementTemplate) {
+    private String convertToJsonString(ElementTemplatesJsonSchema010 elementTemplate) {
         var objectMapper = new ObjectMapper();
         var json = "";
         try {
@@ -89,11 +106,14 @@ public class Camunda7ElementTemplateGenerator implements GenerateElementTemplate
     }
 
     private Property createPropertyWithAnnotation(String label, PropertyType type, String value, ElementTemplateProperty propertyAnnotation) {
-        var property = new Property(label, type, value);
+        var property = new Property();
+        property.setLabel(label);
+        property.setType(type.getType());
+        property.setValue(value);
 
         if (!Objects.isNull(propertyAnnotation)) {
             property.setLabel(propertyAnnotation.label().isEmpty() ? label : propertyAnnotation.label());
-            property.setType(Objects.isNull(propertyAnnotation.type()) ? type : propertyAnnotation.type());
+            property.setType(Objects.isNull(propertyAnnotation.type()) ? type.getType() : propertyAnnotation.type().getType());
             property.setEditable(propertyAnnotation.editable());
 
             var constraints = new Constraints();
