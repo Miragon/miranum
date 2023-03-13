d365368f830f13697a5f8eadf26ce8f3267ca02e
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-13 20:26:23 +0100

    docu: add javadoc to element template property anno

--- connect/connect-core/connect-element-templates/element-template-api/src/main/java/io/miragon/miranum/connect/elementtemplate/api/ElementTemplateProperty.java
+++ connect/connect-core/connect-element-templates/element-template-api/src/main/java/io/miragon/miranum/connect/elementtemplate/api/ElementTemplateProperty.java
@@ -5,12 +5,34 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to customize the property generation during the element template generation.
+ * Is only used in combination with {@link GenerateElementTemplate}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ElementTemplateProperty {
 
+    /**
+     * The type of the property of type {@link PropertyType}.
+     * @return the type of the property
+     */
     PropertyType type();
+    /**
+     * The label of the property.
+     * Corresponds to the label property of the element template property.
+     * @return the label of the property
+     */
     String label() default "";
+    /**
+     * Constraints the property to be not empty.
+     * Adds a constraint to the element template property.
+     * @return true if the property should not be empty, false otherwise
+     */
     boolean notEmpty() default false;
+    /**
+     * Constraints the property to be editable.
+     * @return true if the property should be editable, false otherwise
+     */
     boolean editable() default true;
 }
