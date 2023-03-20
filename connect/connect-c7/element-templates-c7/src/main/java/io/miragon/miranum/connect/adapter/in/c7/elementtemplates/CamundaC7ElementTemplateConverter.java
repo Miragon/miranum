ed553dc1dc12d38a0cf2af194f3dc4d31246fdd8
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-20 09:47:55 +0100

    fix: add schema attribute to json

--- connect/connect-c7/element-templates-c7/src/main/java/io/miragon/miranum/connect/adapter/in/c7/elementtemplates/CamundaC7ElementTemplateConverter.java
+++ connect/connect-c7/element-templates-c7/src/main/java/io/miragon/miranum/connect/adapter/in/c7/elementtemplates/CamundaC7ElementTemplateConverter.java
@@ -6,11 +6,16 @@ import io.miragon.miranum.connect.c7.elementtemplates.gen.CamundaC7ElementTempla
 
 public class CamundaC7ElementTemplateConverter {
 
+    private static final String $SCHEMA = "https://unpkg.com/@camunda/element-templates-json-schema@0.1.0/resources/schema.json";
+
     public static String toJsonString(CamundaC7ElementTemplate elementTemplate) {
         var objectMapper = new ObjectMapper();
+        objectMapper.addMixIn(CamundaC7ElementTemplate.class, SchemaMixin.class);
+        var objectWriter = objectMapper.writerFor(CamundaC7ElementTemplate.class)
+                .withAttribute("$schema", $SCHEMA);
         String json;
         try {
-            json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(elementTemplate);
+            json = objectWriter.withDefaultPrettyPrinter().writeValueAsString(elementTemplate);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Could not generate json string!", e);
         }
---
+++ connect/connect-c7/element-templates-c7/src/main/java/io/miragon/miranum/connect/adapter/in/c7/elementtemplates/SchemaMixin.java
@@ -0,0 +1,13 @@
+package io.miragon.miranum.connect.adapter.in.c7.elementtemplates;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+
+@JsonAppend(
+        attrs = {
+                @JsonAppend.Attr(value = "$schema"),
+        }
+)
+public class SchemaMixin {
+    // Because the generated element template does not have a property to
+    // set the schema, we add it manually on serialization
+}
