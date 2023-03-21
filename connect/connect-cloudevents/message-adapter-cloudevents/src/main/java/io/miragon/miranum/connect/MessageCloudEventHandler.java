ec3b2e3aef905e9509daf5cd158ff0142635fd8f
Julian Haupt <julian@haupt.media>
2023-03-21 22:17:23 +0100

    parse CloudEvent to POJO and pass it to message api

--- connect/connect-cloudevents/message-adapter-cloudevents/src/main/java/io/miragon/miranum/connect/MessageCloudEventHandler.java
+++ connect/connect-cloudevents/message-adapter-cloudevents/src/main/java/io/miragon/miranum/connect/MessageCloudEventHandler.java
@@ -1,11 +1,17 @@
 package io.miragon.miranum.connect;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cloudevents.CloudEvent;
 import io.cloudevents.core.builder.CloudEventBuilder;
+import io.cloudevents.core.data.PojoCloudEventData;
+import io.cloudevents.jackson.PojoCloudEventDataMapper;
 import io.miragon.miranum.connect.cloudevents.CloudEventHandler;
+import io.miragon.miranum.connect.cloudevents.types.PublishMessageRequestSchema;
 import io.miragon.miranum.connect.cloudevents.types.PublishMessageResponseSchema;
 import io.miragon.miranum.connect.json.api.JsonSchema;
 import io.miragon.miranum.connect.json.impl.JsonSchemaFactory;
+import io.miragon.miranum.connect.message.api.CorrelateMessageCommand;
+import io.miragon.miranum.connect.message.api.MessageApi;
 import jakarta.annotation.PostConstruct;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +21,21 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+
+import static io.cloudevents.core.CloudEventUtils.mapData;
 
 @Component
 public class MessageCloudEventHandler implements CloudEventHandler {
     @Autowired
     private ResourceLoader resourceLoader;
+    @Autowired
+    private MessageApi messageApi;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private JsonSchema jsonSchema;
 
@@ -39,21 +53,28 @@ public class MessageCloudEventHandler implements CloudEventHandler {
     @SneakyThrows
     @Override
     public Optional<CloudEvent> handleCloudEvent(CloudEvent cloudEvent) {
-        var validationErrors = jsonSchema.validate(cloudEvent.getData());
+        PojoCloudEventData<PublishMessageRequestSchema> cloudEventData = mapData(
+                cloudEvent,
+                PojoCloudEventDataMapper.from(objectMapper, PublishMessageRequestSchema.class)
+        );
+
+        var validationErrors = jsonSchema.validate(cloudEventData.getValue());
 
         if(!validationErrors.isEmpty()) {
             throw new Exception("Validation Error");
         }
 
+        var publishMessageRequest = cloudEventData.getValue();
 
+        var correlateMessageCommand = new CorrelateMessageCommand();
+        correlateMessageCommand.setMessageName(publishMessageRequest.getName());
+        correlateMessageCommand.setCorrelationKey(publishMessageRequest.getCorrelationKey());
+        correlateMessageCommand.setVariables(objectMapper.readValue(publishMessageRequest.getVariables(), Map.class));
+
+        messageApi.correlateMessage(correlateMessageCommand);
 
         PublishMessageResponseSchema res = new PublishMessageResponseSchema();
         res.setKey(123D);
-
-
-
-        // Todo parse CloudEvent to Java POJO
-
 
         return Optional.of(CloudEventBuilder.v1()
                 .withId(UUID.randomUUID().toString())
