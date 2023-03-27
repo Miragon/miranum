6da3492d22a283ef90a906ee6f2f05a7ca0477c7
Rami Nakkar <rami.nakkar@miragon.io>
2023-03-27 10:00:23 +0200

    refactor: minor pr comments

--- integrations/fit-connect/fit-connect-destination-connector/src/main/java/io/miragon/miraum/fitconnect/integration/destinationconnector/AuthorityProperties.java
+++ integrations/fit-connect/fit-connect-destination-connector/src/main/java/io/miragon/miraum/fitconnect/integration/destinationconnector/AuthorityProperties.java
@@ -1,18 +1,24 @@
 package io.miragon.miraum.fitconnect.integration.destinationconnector;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "fitconnect.subscriber")
 @Getter
 @Setter
+@Validated
 public class AuthorityProperties {
 
+    @NotNull
     private Map<String, String> processDestinationMap;
     private long fixedRate = 60_000;
+    @NotNull
     private String privateKeyDecryptionPath;
+    @NotNull
     private String privateKeySigningPath;
 }
