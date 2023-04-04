4a405901b4cf897d91dc3cfe6208139d5b9fbca8
Rami Nakkar <rami.nakkar@miragon.io>
2023-04-04 14:32:22 +0200

    feat: additional properties

--- connect/connect-core/connect-element-templates-maven-plugin/src/main/java/io/miragon/miranum/connect/elementtemplate/ElementTemplateGeneratorPlugin.java
+++ connect/connect-core/connect-element-templates-maven-plugin/src/main/java/io/miragon/miranum/connect/elementtemplate/ElementTemplateGeneratorPlugin.java
@@ -3,10 +3,14 @@ package io.miragon.miranum.connect.elementtemplate;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.util.Objects;
 
 /**
  * Goal which generates element templates.
@@ -14,13 +18,26 @@ import org.apache.maven.project.MavenProject;
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public class ElementTemplateGeneratorPlugin extends AbstractMojo {
 
+    private final Log log = getLog();
+
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
+    @Parameter(property = "connect-element-template.generator.maven.plugin.output", defaultValue = "${project.build.directory}")
+    private File outputDirectory;
+
+    @Parameter(name = "skip", property = "elementtemplategen.skip", defaultValue = "false")
+    private Boolean skip;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("*** Generate element templates BEGIN ***");
+        if (Objects.nonNull(skip) && skip) {
+            getLog().info("Element-template generation is skipped.");
+            return;
+        }
 
-        getLog().info("*** Generate element templates END ***");
+        log.info("*** Generate element templates BEGIN ***");
+
+        log.info("*** Generate element templates END ***");
     }
 }
