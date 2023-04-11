ffbd2d14b1ffe6d9021361a6b9873c4578326f49
Rami Nakkar <rami.nakkar@miragon.io>
2023-04-11 14:34:51 +0200

    feat: add reflection

--- connect/connect-core/connect-element-templates-maven-plugin/src/main/java/io/miragon/miranum/connect/elementtemplate/ElementTemplateGeneratorPlugin.java
+++ connect/connect-core/connect-element-templates-maven-plugin/src/main/java/io/miragon/miranum/connect/elementtemplate/ElementTemplateGeneratorPlugin.java
@@ -1,5 +1,7 @@
 package io.miragon.miranum.connect.elementtemplate;
 
+import io.miragon.miranum.connect.elementtemplate.api.GenerateElementTemplate;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -8,9 +10,20 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * Goal which generates element templates.
@@ -37,6 +50,38 @@ public class ElementTemplateGeneratorPlugin extends AbstractMojo {
         }
 
         log.info("*** Generate element templates BEGIN ***");
+
+        try {
+            List<String> classpathElements = project.getCompileClasspathElements();
+            List<URL> classpathURLs = new ArrayList<>(classpathElements.size());
+
+            for (String element : classpathElements) {
+                try {
+                    classpathURLs.add(new URL("file://" + element));
+                } catch (MalformedURLException e) {
+                    throw new MojoExecutionException("Malformed classpath element: " + element, e);
+                }
+            }
+
+            project.addCompileSourceRoot(outputDirectory.getAbsolutePath());
+
+            URLClassLoader urlClassLoader = new URLClassLoader(classpathURLs.toArray(new URL[0]), getClass().getClassLoader());
+
+            Reflections reflections = new Reflections(new ConfigurationBuilder()
+                    .setUrls(ClasspathHelper.forClassLoader(urlClassLoader))
+                    .addClassLoader(urlClassLoader)
+                    .addScanners(new MethodAnnotationsScanner())); // Add MethodAnnotationsScanner
+
+            Set<Method> annotatedMethods = reflections.getMethodsAnnotatedWith(GenerateElementTemplate.class);
+            // Replace 'MyMethodAnnotation' with your custom method annotation class
+
+            for (Method method : annotatedMethods) {
+                // Process the annotated methods
+                log.info("Found annotated method: " + method.getName());
+            }
+        } catch (DependencyResolutionRequiredException e) {
+            throw new MojoExecutionException("Dependency resolution failed", e);
+        }
 
         log.info("*** Generate element templates END ***");
     }
