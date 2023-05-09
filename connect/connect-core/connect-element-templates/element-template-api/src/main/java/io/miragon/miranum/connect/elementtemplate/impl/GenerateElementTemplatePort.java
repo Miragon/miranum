8c0491946cce36130274724aa6ec7fea3c42457d
Rami Nakkar <58343300+rami-nk@users.noreply.github.com>
2023-05-09 17:12:56 +0200

    refactor(element-templates): create maven plugin to generate element templates on build (#122)
    **Issue**
    Element templates are generated on runtime.
    
    
    **Description**
    The current element template generation is refactored and moved to a
    maven plugin that is executed during the build. The element template
    generation can be further customized by specifying an outputDirectory
    and targetPlatforms (eg. camunda7).
    
    **Checklist**
    
    * Code is easy to understand
    * No obvious errors or bugs
    * CI/CD Pipelines did run successfully
    * Tests were implemented
    * Documentation was created
    
    ---------
    
    Co-authored-by: Dominik Horn <dominik.horn93@gmail.com>
    Co-authored-by: peterhnm <peter.hnm@gmx.de>

--- connect/connect-core/connect-element-templates/element-template-api/src/main/java/io/miragon/miranum/connect/elementtemplate/impl/GenerateElementTemplatePort.java
+++
@@ -1,6 +0,0 @@
-package io.miragon.miranum.connect.elementtemplate.impl;
-
-public interface GenerateElementTemplatePort {
-
-    ElementTemplateGenerationResult generate(ElementTemplateInfo elementTemplateInfo);
-}
--- examples/simple-example/simple-example-core/src/main/java/io/miragon/miranum/integrations/example/application/service/SendMessageService.java
+++ examples/simple-example/simple-example-core/src/main/java/io/miragon/miranum/integrations/example/application/service/SendMessageService.java
@@ -1,7 +1,7 @@
 package io.miragon.miranum.integrations.example.application.service;
 
 import io.miragon.miranum.connect.elementtemplate.api.BPMNElementType;
-import io.miragon.miranum.connect.elementtemplate.api.GenerateElementTemplate;
+import io.miragon.miranum.connect.elementtemplate.api.ElementTemplate;
 import io.miragon.miranum.connect.message.api.CorrelateMessageCommand;
 import io.miragon.miranum.connect.message.api.MessageApi;
 import io.miragon.miranum.connect.worker.api.Worker;
@@ -11,8 +11,6 @@ import io.miragon.miranum.integrations.example.domain.Answer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 
-import java.util.Map;
-
 @Log
 @RequiredArgsConstructor
 public class SendMessageService implements SendMessageUseCase {
@@ -21,11 +19,7 @@ public class SendMessageService implements SendMessageUseCase {
 
     @Override
     @Worker(type = "sendMessage")
-    @GenerateElementTemplate(name = "Send Message",
-            id = "send-message",
-            type = "sendMessage",
-            appliesTo = {BPMNElementType.BPMN_SERVICE_TASK},
-            version = "0-1")
+    @ElementTemplate(name = "Send Message", description = "Send a message.")
     public Answer sendMessage(final SendMessageCommand message) {
         log.info("Received message: " + message);
         messageApi.correlateMessage(new CorrelateMessageCommand(message.getContent(), message.getKey()));
