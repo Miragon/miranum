e9b42c8744f940e7364050b75c67b9b951bb118d
Dominik Horn <dominik.horn93@gmail.com>
2023-05-09 11:01:42 +0200

    refactor(element-tempates): element templates c8 (#135)

---
+++ connect/connect-element-templates-maven-plugin/element-templates-c8/src/main/java/io/miragon/miranum/connect/elementtemplate/c8/SchemaMixin.java
@@ -0,0 +1,10 @@
+package io.miragon.miranum.connect.elementtemplate.c8;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+
+@JsonAppend(attrs = @JsonAppend.Attr(value = "$schema"), prepend = true)
+public class SchemaMixin
+{
+    // Because the generated element template does not have a property to
+    // set the schema, we add it manually on serialization
+}
