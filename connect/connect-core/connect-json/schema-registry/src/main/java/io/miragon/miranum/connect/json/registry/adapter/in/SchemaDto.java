2b4d900779b0f459de06a41a8de677923eb338b4
Dominik Horn <dominik.horn93@gmail.com>
2023-05-24 15:45:39 +0200

    fix(schema-registry): fix auto config (#143)
    **Issue**
    
    
    
    **Description**
    
    
    
    **Checklist**
    
    * Code is easy to understand
    * No obvious errors or bugs
    * CI/CD Pipelines did run successfully
    * Tests were implemented
    * Documentation was created

--- connect/connect-core/connect-json/schema-registry/src/main/java/io/miragon/miranum/connect/json/registry/adapter/in/SchemaDto.java
+++ connect/connect-core/connect-json/schema-registry/src/main/java/io/miragon/miranum/connect/json/registry/adapter/in/SchemaDto.java
@@ -3,26 +3,30 @@ package io.miragon.miranum.connect.json.registry.adapter.in;
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SchemaDto {
     // All artefacts of one process are organized in a bundle.
     @NotNull
     @Size(min = 1, max = 255)
-    private final String bundle;
+    private String bundle;
 
     // The name of the schema.
     @NotNull
     @Size(min = 1, max = 255)
-    private final String ref;
+    private String ref;
 
     // The tag of the schema.
     @NotNull
     @Size(min = 1, max = 255)
-    private final String tag;
+    private String tag;
 
     // The schema content.
     @NotNull
-    private final JsonNode jsonNode;
+    private JsonNode jsonNode;
 }
