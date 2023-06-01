fd3e27810fefeedb481c89a1fc71279cb3722519
Andreas Riepl <andreas.riepl@miragon.io>
2023-06-01 10:46:22 +0200

    feat: use muenchen/vjsf

--- apps/packages/apps/digiwf-tasklist/src/plugins/vjsf.ts
+++ apps/packages/apps/digiwf-tasklist/src/plugins/vjsf.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 // eslint-disable-next-line
 //@ts-ignore
-import VJsf from '@miragon/vjsf/lib/VJsf.js';
-import '@miragon/vjsf/lib/VJsf.css';
-import '@miragon/vjsf/lib/deps/third-party.js';
+import VJsf from '@muenchen/vjsf/lib/VJsf.js';
+import '@muenchen/vjsf/lib/VJsf.css';
+import '@muenchen/vjsf/lib/deps/third-party.js';
 import 'easymde/dist/easymde.min.css';
 import EasyMDE from 'easymde/dist/easymde.min.js';
 // eslint-disable-next-line
