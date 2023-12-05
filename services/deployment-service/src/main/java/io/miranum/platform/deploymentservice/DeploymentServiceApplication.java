12ad018450972f994f2a59e56e682357b20e01f8
Lukas Mösle <37705369+lmoesle@users.noreply.github.com>
2023-12-05 09:06:03 +0100

    feature: add miranum deployment service (#72)
    **Changes**
    
    - Add miranum-platform-deployment-service to reduce release dependencies
    to miranum-ide
    - Lifecycle

---
+++ services/deployment-service/src/main/java/io/miranum/platform/deploymentservice/DeploymentServiceApplication.java
@@ -0,0 +1,14 @@
+package io.miranum.platform.deploymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DeploymentServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(DeploymentServiceApplication.class, args);
+    }
+
+}
