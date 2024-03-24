a425581c6e1ce1b1fb2112f51f8935f0b80695c7
dominikhorn <dominik.horn93@gmail.com>
2024-03-24 19:02:08 +0100

    refactor: fix naming

--- platform/engine/core/src/main/java/io/miranum/platform/engine/application/port/in/process/GetFileNamesForProcessStartUseCase.java
+++
@@ -1,9 +0,0 @@
-package io.miranum.platform.engine.application.port.in.process;
-
-import java.util.List;
-
-public interface GetFileNamesForProcessStartUseCase {
-
-    List<String> getFileNames(final String definitionKey, final String filePath, final String userId, final List<String> groups);
-
-}
---
+++ platform/engine/task/src/main/java/io/miranum/platform/tasklist/adapter/in/process/StartInstanceListener.java
@@ -0,0 +1,6 @@
+package io.miranum.platform.tasklist.adapter.in.process;
+
+public class StartInstanceListener {
+
+    //has to use the Execution of camunda
+}
