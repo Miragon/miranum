6431ef212bb86934c0318e9ce7d86bc8c8e882d9
itdlz-rian <andreas.riepl@ldbv.bayern.de>
2024-05-29 11:35:06 +0200

    fix tests

--- connect/camunda7-remote/utils/src/test/java/io/miragon/miranum/connect/camnda7/remote/utils/Camunda7PojoMapperTest.java
+++ connect/camunda7-remote/utils/src/test/java/io/miragon/miranum/connect/camnda7/remote/utils/Camunda7PojoMapperTest.java
@@ -1,6 +1,8 @@
 package io.miragon.miranum.connect.camnda7.remote.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.camunda.bpm.client.variable.ClientValues;
+import org.camunda.bpm.client.variable.impl.value.JsonValueImpl;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
@@ -53,37 +55,31 @@ public class Camunda7PojoMapperTest {
 
     @Test
     public void testMapFromEngineData_JsonObjectValue_ShouldReturnMap() {
-        String jsonValue = "{\"key\":\"value\"}";
         // Given a VariableMap with a JSON object value
-        VariableMap variableMap = Variables.createVariables()
-                .putValueTyped("var1", Variables
-                        .objectValue(jsonValue)
-                        .serializationDataFormat(Variables.SerializationDataFormats.JSON)
-                        .create());
+        VariableMap variableMap = Variables
+                .createVariables()
+                .putValueTyped("var1", new JsonValueImpl("{\"key\":\"value\"}"));
 
         // When
         Map<String, Object> result = mapper.mapFromEngineData(variableMap);
 
         // Then
         assertEquals(1, result.size());
-        assertEquals(jsonValue, result.get("var1"));
+        assertEquals(Map.of("key", "value"), result.get("var1"));
     }
 
     @Test
     public void testMapFromEngineData_JsonArrayToList() {
         // Given a VariableMap with a JSON list
-        String jsonArray = "[{\"key\":\"value1\"}, {\"key2\":\"value2\"}]";
-        VariableMap variableMap = Variables.createVariables()
-                .putValueTyped("var1", Variables
-                        .objectValue(jsonArray)
-                        .serializationDataFormat(Variables.SerializationDataFormats.JSON)
-                        .create());
+        VariableMap variableMap = Variables
+                .createVariables()
+                .putValueTyped("var1", new JsonValueImpl("[{\"key\":\"value1\"}, {\"key2\":\"value2\"}]"));
 
         // When
         Map<String, Object> result = mapper.mapFromEngineData(variableMap);
 
         // Then
-        assertEquals(jsonArray, result.get("var1"));
+        assertEquals(List.of(Map.of("key", "value1"), Map.of("key2", "value2")), result.get("var1"));
     }
 
 }
