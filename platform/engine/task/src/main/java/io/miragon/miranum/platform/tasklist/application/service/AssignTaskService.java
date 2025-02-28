108dc3fad3c01f64a09c3fe0356a50773437ca32
Lukas Mösle <lukas95m@yahoo.de>
2025-02-28 14:50:09 +0100

    refactoring: remove miranum-connect from miranum platform

--- platform/engine/task/src/main/java/io/miragon/miranum/platform/tasklist/application/service/AssignTaskService.java
+++ platform/engine/task/src/main/java/io/miragon/miranum/platform/tasklist/application/service/AssignTaskService.java
@@ -1,29 +1,28 @@
 package io.miragon.miranum.platform.tasklist.application.service;
 
 import io.miragon.miranum.auth.api.UserAuthenticationProvider;
-import io.miragon.miranum.connect.task.api.TaskApi;
-import io.miragon.miranum.connect.task.api.command.AssignUserTaskCommand;
 import io.miragon.miranum.platform.tasklist.application.port.in.WorkOnTaskUseCase;
+import io.miragon.miranum.platform.tasklist.application.port.out.task.AssignTaskOutPort;
+import io.miragon.miranum.platform.tasklist.domain.AssignUserTask;
 import io.miragon.miranum.platform.tasklist.exception.TaskAccessDeniedException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class AssignTaskService implements WorkOnTaskUseCase {
 
-    private final TaskApi connectTaskApi;
+    private final AssignTaskOutPort assignTaskOutPort;
     private final UserAuthenticationProvider userAuthenticationProvider;
 
     @Override
     public void assignUserTask(String user, String taskId, String assignee) throws TaskAccessDeniedException {
-        final AssignUserTaskCommand command = AssignUserTaskCommand.builder()
+        assignTaskOutPort.assignUserTask(AssignUserTask.builder()
             .taskId(taskId)
             .assignee(assignee)
-            .build();
-        connectTaskApi.assignUserTask(command, user, userAuthenticationProvider.getLoggedInUserRoles());
+            .build(), user, userAuthenticationProvider.getLoggedInUserRoles());
     }
 
     @Override
     public void unassignUserTask(String user, String taskId) throws TaskAccessDeniedException {
-        connectTaskApi.unassignUserTask(taskId, null, userAuthenticationProvider.getLoggedInUserRoles());
+        assignTaskOutPort.unassignUserTask(taskId, null, userAuthenticationProvider.getLoggedInUserRoles());
     }
 }
